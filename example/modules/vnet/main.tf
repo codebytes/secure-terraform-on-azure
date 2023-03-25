eac31a28eccffa1bd5fbc678676b26640009ce36
Chris Ayers <clayers@gmail.com>
2023-03-25 16:26:43 +0000

    added nsg

--- example/modules/vnet/main.tf
+++ example/modules/vnet/main.tf
@@ -2,11 +2,18 @@ data "azurerm_resource_group" "rg" {
   name = var.rg_name
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-network"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
+  security_group_id   = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_subnet" "snet" {
